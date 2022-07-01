e742914e05fa51f70333f30fd286496efc019b9f
roger chat <mattrogerpelosi@gmail.com>
2022-07-01 13:22:50 -0400

    removing /dist from .gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HandsComponent } from './components/hands/hands.component';
+import { PenComponent } from './components/pen/pen.component';
+import { KeyboardComponent } from './components/keyboard/keyboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HandsComponent,
+    PenComponent,
+    KeyboardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/hands/hands.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HandsComponent } from './hands.component';
+
+describe('HandsComponent', () => {
+  let component: HandsComponent;
+  let fixture: ComponentFixture<HandsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HandsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HandsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
