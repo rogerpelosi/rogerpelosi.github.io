ce3a32b38badc649d87202c0ebc59e0d08913a5c
roger chat <mattrogerpelosi@gmail.com>
2022-07-01 13:35:17 -0400

    transfering navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {Routes, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HandsComponent } from './components/hands/hands.component';
 import { PenComponent } from './components/pen/pen.component';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'pen', component: PenComponent},
+  {path: 'hands', component: HandsComponent},
+  {path: 'keyboard', component: KeyboardComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -13,10 +24,12 @@ import { KeyboardComponent } from './components/keyboard/keyboard.component';
     HomeComponent,
     HandsComponent,
     PenComponent,
-    KeyboardComponent
+    KeyboardComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
